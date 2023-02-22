205daf3de79678f35350c1c91c13762978181e4f
Rohit Kumar Pandit <77102068+rohit-64bit@users.noreply.github.com>
2023-02-22 21:25:52 +0530

    Theme Issue  Fix  Done

--- memories/src/components/Chat/ChatSection.jsx
+++ memories/src/components/Chat/ChatSection.jsx
@@ -5,6 +5,29 @@ import { Avatar, IconButton, ListItemIcon, Menu, MenuItem, Tooltip } from '@mui/
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import ReportProblemIcon from '@mui/icons-material/ReportProblem';
 import PersonIcon from '@mui/icons-material/Person';
+import SendIcon from '@mui/icons-material/Send';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+
+
+const Message = (props) => {
+    return (
+        <>
+            <div className='bg-[#8948B8] text-white w-max max-w-[70%] px-3 py-1 rounded-t-full rounded-r-full shadow-lg dark:shadow-black flex gap-1'>
+                <div>
+                    Lorem, ipsum dolor sit amet consec
+                </div>
+                {/* <div className='text-xs opacity-40 '>00:00</div> */}
+            </div>
+            <div className='bg-[#FFFFFF] w-max max-w-[70%] px-3 py-1 rounded-t-full rounded-l-full self-end shadow-lg dark:shadow-black flex gap-1'>
+                <div>
+                    Lorem
+                </div>
+                {/* <div className='text-xs opacity-40 '>00:00</div> */}
+            </div>
+        </>
+    )
+}
+
 
 const ChatSection = () => {
     const [anchorEl, setAnchorEl] = useState(null);
@@ -67,8 +90,30 @@ const ChatSection = () => {
                         </div>
                     </Menu>
                 </div>
-                <div className='h-[71vh] w-full flex flex-cols'>
+                <div className='h-[71vh] w-full p-5 flex flex-col'>
+                    <div className='h-full w-full overflow-y-auto my-2 flex gap-5 flex-col p-2'>
 
+                        <Message />
+                        <Message />
+                        <Message />
+                        <Message />
+                        <Message />
+                        <Message />
+
+                    </div>
+                    <form method='POST' className='bg-white border-[4px] dark:bg-[#231344] dark:border-[#33215A] dark:text-white border-[#D4D4D4] w-full h-max flex rounded-lg'>
+
+                        <IconButton className='dark:text-white dark:opacity-60 duration-300'>
+                            <EmojiEmotionsIcon />
+                        </IconButton>
+
+                        <input type="text" className='w-full bg-transparent p-2' />
+
+                        <IconButton type='submit' className='dark:text-white dark:opacity-60 duration-300'>
+                            <SendIcon />
+                        </IconButton>
+
+                    </form>
                 </div>
             </div>
         </>
