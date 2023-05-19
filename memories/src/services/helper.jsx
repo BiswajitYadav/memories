f8913f802843161f9c3b922c2a9cda176cd79411
Rohit Kumar Pandit <77102068+rohit-64bit@users.noreply.github.com>
2023-05-20 00:31:02 +0530

    .

--- memories/src/services/helper.jsx
+++ memories/src/services/helper.jsx
@@ -1,6 +1,6 @@
 // export const SERVER_URL = "http://localhost:8888/api/"
 
-// export const IO_SERVER_URL = "http://localhost:9000"
+// export const IO_SERVER_URL = "http://localhost:8888"
 
-export const IO_SERVER_URL = "https://memories-backend-legacy.onrender.com:9000"
+export const IO_SERVER_URL = "https://memories-backend-legacy.onrender.com:10000"
 export const SERVER_URL = "https://memories-backend-legacy.onrender.com/api/"
