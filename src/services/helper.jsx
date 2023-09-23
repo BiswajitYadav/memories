31c99ebbcee46e6ec6fa222ea42430e6fd76479a
Rohit Kumar Pandit <77102068+rohit-64bit@users.noreply.github.com>
2023-09-23 13:33:39 +0530

    netlify deploy

--- src/services/helper.jsx
+++ src/services/helper.jsx
@@ -3,6 +3,10 @@ import io from 'socket.io-client'
 // export const SERVER_URL = "http://localhost:8888/api/"
 // export const IO_SERVER_URL = "http://localhost:8888"
 
-export const IO_SERVER_URL = "https://api.thememories.social"
-export const SERVER_URL = "https://api.thememories.social/api/"
+// export const IO_SERVER_URL = "https://api.thememories.social"
+// export const SERVER_URL = "https://api.thememories.social/api/"
+
+export const IO_SERVER_URL = "https://memories-backend-legacy.onrender.com/"
+export const SERVER_URL = "https://memories-backend-legacy.onrender.com/api/"
+
 export const socket = io.connect(`${IO_SERVER_URL}`)
